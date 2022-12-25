5ff401471bf886e6984958996a88ad3099d95040
Alizeh Khan <59119022+alizehkhan@users.noreply.github.com>
2022-12-25 12:50:40 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,7 @@
+alias edit="code ~/.zshrc"
 alias python="python3 $@"
-alias run="python3 manage.py runserver"
+alias killdjango="sudo lsof -t -i tcp:8000 | xargs kill -9"
+
+function run() {
+  parallel -j 2 --linebuffer --tagstring '\033[3{#}m[{}]\033[0m' ::: '' '' '' 'cd map && yarn watch' '' 'python3 manage.py runserver'
+}
