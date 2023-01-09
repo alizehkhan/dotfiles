1d1b7c86b18154414fa7925de28c2dabd30080b9
Alizeh Khan <59119022+alizehkhan@users.noreply.github.com>
2023-01-09 14:52:49 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,13 @@
+# Open zshrc in VS Code
 alias edit="code ~/.zshrc"
-alias python="python3 $@"
-alias killdjango="sudo lsof -t -i tcp:8000 | xargs kill -9"
 
-function run() {
-  parallel -j 2 --linebuffer --tagstring '\033[3{#}m[{}]\033[0m' ::: '' '' '' 'cd map && yarn watch' '' 'python3 manage.py runserver'
+# Kill server running on this port
+alias kill8000="sudo lsof -t -i tcp:8000 | xargs kill -9"
+alias kill3000="sudo lsof -t -i tcp:3000 | xargs kill -9"
+
+alias seed="./manage.py seed"
+
+function wandern() {
+  cd ~/code/wandern
+  parallel -j 2 --linebuffer --tagstring '\033[3{#}m[{#}]\033[0m' ::: '' '' '' 'cd frontend && yarn start' '' 'cd api && python3 manage.py runserver'
 }
